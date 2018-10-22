bcc22e675b048d93917fe57fa72acb3ddbae9335
Валерий Коканов <vkokanov@alfabank.ru>
2018-10-22 17:24:08 +0300

    Initial Commit

---
+++ OrganizationsMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrganizationsMap
+//
+//  Created by Валерий Коканов on 22/10/2018.
+//  Copyright © 2018 my-night. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OrganizationsMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OrganizationsMap
+//
+//  Created by Валерий Коканов on 22/10/2018.
+//  Copyright © 2018 my-night. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
